8471207305a042e12fc1ddf795fbbd345ea0cf3f
zerok06 <jose.geeksjose@gmail.com>
2024-10-07 23:05:54 -0500

    add: adding navbar and wrapper

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: "#4d09ed"
+				},
+				low: {
+					DEFAULT: "#6445dd"
+				},
+				hard: {
+					DEFUALT: "#1a05af"
+				}
+			},
+			fontFamily: {
+				sans: ['Outfit', ...defaultTheme.fontFamily.sans],
+			},
+
+		},
 	},
 	plugins: [],
 }
